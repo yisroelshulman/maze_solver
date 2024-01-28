db71694383b3187dd6feaa7292d707fc023326af
yisroel <yisroelshulman16@gmail.com>
2024-01-28 00:26:33 -0500

    refactored

---
+++ gui.py
@@ -0,0 +1,42 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color='red'):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.pack()
+
+class Window:
+    def __init__(self, w, h):
+        self.__width = w
+        self.__height = h
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, height=self.__height, width=self.__width)
+        self.__canvas.pack()
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("closing window")
+
+    def close(self):
+        self.__running = False
+
+    def draw_line(self, line: Line, fill_color="red"):
+        line.draw(self.__canvas, fill_color)
