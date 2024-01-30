f5a95ba6a4c7a63a5cc02a65ef97afbe84d18aaa
yisroel <yisroelshulman16@gmail.com>
2024-01-30 13:17:17 -0500

    refactored the cell.py document

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
-from gui import Line, Point
+from gui import Line, Point, Window
 
 class Cell:
-    def __init__(self, win=None): # x1 < x2, y1 < y2
+    def __init__(self, win: Window=None) -> None:
         self._x1 = None
         self._x2 = None
         self._y1 = None
@@ -13,7 +13,19 @@ class Cell:
         self.visited = False
         self._win = win
 
-    def draw_walls(self, x1, y1, x2 , y2):
+    def _draw_wall(self, wall: Line, has_wall: bool) -> None:
+        if has_wall:
+            self._win.draw_line(wall)
+        else:
+            self._win.draw_line(wall, '#d9d9d9') # the default tkinter background color on windows
+
+    # The 4 parameters have the following correspondence
+    # x1 = left most x position
+    # y1 = top most y position
+    # x2 = right most x position
+    # y2 = bottom most y position
+    # this is because the windows top left corner has coords (0, 0)
+    def draw_walls(self, x1: float, y1: float, x2: float , y2: float) -> None:
         if self._win is None:
             return
         self._x1 = x1
@@ -28,30 +40,19 @@ class Cell:
         right_wall = Line(top_right, bottom_right)
         top_wall = Line(top_left, top_right)
         bottom_wall = Line(bottom_left, bottom_right)
-        if self.has_left_wall:
-            self._win.draw_line(left_wall)
-        else:
-            self._win.draw_line(left_wall, "#d9d9d9")
-        if self.has_right_wall:
-            self._win.draw_line(right_wall)
-        else:
-            self._win.draw_line(right_wall, "#d9d9d9")
-        if self.has_top_wall:
-            self._win.draw_line(top_wall)
-        else:
-            self._win.draw_line(top_wall, "#d9d9d9")
-        if self.has_bottom_wall:
-            self._win.draw_line(bottom_wall)
-        else:
-            self._win.draw_line(bottom_wall, "#d9d9d9")
+        self._draw_wall(left_wall, self.has_left_wall)
+        self._draw_wall(right_wall, self.has_right_wall)
+        self._draw_wall(top_wall, self.has_top_wall)
+        self._draw_wall(bottom_wall, self.has_bottom_wall)
 
-    def center(self):
+    def _center(self) -> Point:
         return Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
 
-    def draw_move(self, to_cell, undo=False):
+    # undo represents backtracking on a path
+    def draw_move(self, to_cell: 'Cell', undo=False) -> None:
         if self._win is None:
             return
-        color = "red"
+        color = 'red'
         if undo:
-            color = "gray"
-        self._win.draw_line(Line(self.center(), to_cell.center()), color)
+            color = 'white'
+        self._win.draw_line(Line(self._center(), to_cell._center()), color)
