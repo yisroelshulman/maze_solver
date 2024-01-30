fe830aa244e5321388b8ed84ef247f3abb64b5fb
yisroel <yisroelshulman16@gmail.com>
2024-01-30 12:22:37 -0500

    added type hints and refactored the gui.py file

--- gui.py
+++ gui.py
@@ -1,43 +1,42 @@
 from tkinter import Tk, BOTH, Canvas
-import time
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
 
 class Line:
-    def __init__(self, p1: Point, p2: Point):
-        self.p1 = p1
-        self.p2 = p2
+    def __init__(self, p1: Point, p2: Point) -> None:
+        self._p1 = p1
+        self._p2 = p2
 
-    def draw(self, canvas: Canvas, fill_color='black'):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
-        canvas.pack()
+    def draw(self, canvas: Canvas, fill_color='black') -> None:
+        canvas.create_line(self._p1.x, self._p1.y, self._p2.x, self._p2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
 
 class Window:
-    def __init__(self, w, h):
-        self.__width = w
-        self.__height = h
+    def __init__(self, width: int, height: int) -> None:
+        self.__width = width
+        self.__height = height
         self.__root = Tk()
         self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root, height=self.__height, width=self.__width)
-        self.__canvas.pack()
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
         self.__running = True
         while self.__running:
             self.redraw()
         print("closing window")
 
-    def close(self):
+    def close(self) -> None:
         self.__running = False
 
-    def draw_line(self, line: Line, fill_color="black"):
+    def draw_line(self, line: Line, fill_color="black") -> None:
         line.draw(self.__canvas, fill_color)
